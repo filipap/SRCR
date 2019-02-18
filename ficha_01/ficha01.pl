9acaa1cb64336e79baf35fd3193df2949562c716
filipap <a82145@alunos.uminho.pt>
2019-02-18 12:17:20 +0000

    update work

---
+++ ficha_01/ficha01.pl
@@ -0,0 +1,67 @@
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% SIST. REPR. CONHECIMENTO E RACIOCINIO - MiEI/3 (ficha pratica 01)
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Base de Conhecimento com informacao genealogica. 
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% SICStus PROLOG: Declaracoes iniciais
+
+:- set_prolog_flag( discontiguous_warnings,off ).
+:- set_prolog_flag( single_var_warnings,off ).
+:- set_prolog_flag( unknown,fail ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado filho: Filho,Pai -> {V,F}
+
+filho( joao,jose ).
+filho( jose,manuel ).
+filho( carlos,jose ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado pai: Pai,Filho -> {V,F}
+
+pai( P,F ) :-
+    filho( F,P ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado avo: Avo,Neto -> {V,F}
+
+avo(A,N) :-
+	pai(A,P),
+	pai(P,N).
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado bisavo: Bisavo,Bisneto -> {V,F}
+
+bisavo(B,BN) :-
+	pai(B,A),
+	pai(A,P),
+	pai(P,BN).
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado descendente: Descendente,Ascendente -> {V,F}
+
+descendente(D,A) :-
+	filho(D,A).
+
+
+ascendente(D,A) :-
+	pai(D,A).
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado descendente: Descendente,Ascendente,Grau -> {V,F}
+
+descendente(D,A,1) :-
+	filho(D,A).
+
+descendente(D,A,G) :-  % recursividade prolog 
+	filho(D,X),			% + não é a função adicao, apenas é uma expressão para ser usada na recursividade
+	descendente(X,A,N),
+	G is N+1.
+
+
+
